49dc9e50203a63bd8af0cf7aad4ef9e272f726e6
atsushi.setosaki <atsuboo.h2o@gmail.com>
2021-03-15 20:52:39 +0900

    add dao

---
+++ src/models/Tasklist.java
@@ -0,0 +1,69 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@NamedQueries({
+    @NamedQuery(
+            name="getAllTasks",
+            query="SELECT m FROM tasks AS m ORDER BY m.id DESC")
+
+})
+@Table(name="tasks")
+public class Tasklist {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="content",length =255 , nullable =false)
+    private String content;
+
+    @Column(name="created_at",nullable = false)
+    private Timestamp created_at;
+
+    @Column(name="updated_at",nullable = false)
+    private Timestamp updated_at;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+}
